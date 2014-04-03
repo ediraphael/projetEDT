e47721c87ad99bbb5ab035583b56bc5851d66566
geeknbar <dorian.coffinet@gmail.com>
2014-04-03 11:19:15 +0200

    Ajout de la classe DAO pour les groupes.

---
+++ src/model/dao/GroupDAO.java
@@ -0,0 +1,47 @@
+package model.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+
+import model.org.persistence.UserEntity;
+
+public class GroupDAO {
+
+	//nom de la database
+	private final static String JPA_DATABASE = "ProjetEDT";
+	//Cette entité permet d'acceder aux tables
+	@PersistenceContext
+	private EntityManager em;
+	
+	
+	/**
+	 * Methode permetant de sauvegarder un user
+	 * @param name
+	 */
+	public void addGroup(String name)
+	{
+		em = Persistence.createEntityManagerFactory(JPA_DATABASE).createEntityManager();
+
+		try 
+		{
+
+		} finally 
+		{
+			getEntityManager().close();
+		}
+	}
+	
+	/**
+	 * Permet de récupérer et d'initialiser l'entity manager si celui ci est null
+	 * @return
+	 */
+	protected EntityManager getEntityManager() 
+	{
+		if (em == null) 
+		{
+			em = Persistence.createEntityManagerFactory(JPA_DATABASE).createEntityManager();
+		}
+		return em;
+	}
+}
