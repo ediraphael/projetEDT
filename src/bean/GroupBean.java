baf132792c6eb539ebc29150e60cf73df3afc26c
geeknbar <dorian.coffinet@gmail.com>
2014-04-03 11:25:05 +0200

    Ajout des attributs et des getter setter du bean de groupe.

--- src/bean/GroupBean.java
+++ src/bean/GroupBean.java
@@ -1,5 +1,13 @@
 package bean;
 
 public class GroupBean {
+	private String name;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
