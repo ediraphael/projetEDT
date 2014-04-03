83ace46ee688dd7792cdbea468502252f76a4ef5
ediraphael <raphael.pillie@gmail.com>
2014-04-03 11:02:47 +0200

    Suppression des warnings dans ClassroomDAOTest

--- src/unitCase/model/dao/ClassroomDAOTest.java
+++ src/unitCase/model/dao/ClassroomDAOTest.java
@@ -1,15 +1,9 @@
 package unitCase.model.dao;
 
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import model.dao.ClassroomDAO;
-import model.dao.ScheduleDAO;
-import model.dao.UserDAO;
 import model.org.persistence.ClassroomEntity;
-import model.org.persistence.ScheduleEntity;
 
 import org.junit.Test;
 
