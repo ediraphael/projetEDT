1cba78816b1cd5a5fc9bb0019482950e59fa2a79
Thibault GAUTHIER <gauthierthibault@live.fr>
2014-04-03 11:17:38 +0200

    Modification du fichier HoraireAction. (2)

--- src/actions/horaire/HoraireAction.java
+++ src/actions/horaire/HoraireAction.java
@@ -1,21 +1,13 @@
 package actions.horaire;
 
-import java.util.Map;
+import actions.abstractAction.AbstractAction;
 
-import org.apache.struts2.interceptor.SessionAware;
-
-import com.opensymphony.xwork2.ActionSupport;
-
-public class HoraireAction extends ActionSupport implements SessionAware {
+public class HoraireAction extends AbstractAction {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	//forward pour rediriger vers la bonne page
-	private String forward;
-	//variable de session
-	private Map<String, Object> session;
 
 	/**
 	 * Execution de l'ajout d'un horaire
@@ -23,26 +15,16 @@ public class HoraireAction extends ActionSupport implements SessionAware {
 	public String execute()
 	{
 		//Sauf si il y a erreur, le traitement est considéré comme étant un succès
-		forward = "SUCCESS";
+		forward = FORWARD_SUCCESS;
 		try
 		{
 
 		}
 		catch(Exception e)
 		{
-			forward="ERROR";
+			forward = FORWARD_ERROR;
 		}
 		return forward;
-	}
-	
-	@Override
-	public void setSession(Map<String, Object> session) {
-		this.session = session;
-	}
-	
-	public Map<String, Object> getSession() 
-	{
-		return session;
 	}
 
 }
